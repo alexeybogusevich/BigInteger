67f501ab36cd628ca76fea0364bac7b0154d2de4
alexeybogusevich <alexey.bogusevich@gmail.com>
2019-10-22 22:28:27 +0300

    initial commit

---
+++ BigIntegerImplementation/BigNumber.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BigIntegerImplementation
+{
+    public class BigNumber
+    {
+        string number;
+        bool negative;
+
+        public BigNumber(string s)
+        {
+            negative = false;
+            int i = 0;
+            if (s[0] == '-')
+            {
+                negative = true;
+                ++i;
+            }
+            while (i < s.Length - 1 && s[i] == '0')
+                ++i;
+            number = s.Substring(i);
+        }
+
+        public BigNumber(string s, bool sign)
+        {
+            int i = 0;
+            while (i < s.Length - 1 && s[i] == '0')
+                i++;
+            number = s.Substring(i);
+            negative = sign;
+        }
+
+
+        public string GetNumber()
+        {
+            if (negative)
+                return "-" + number;
+            else
+                return number;
+        }
+
+        public static BigNumber operator +(BigNumber a, BigNumber b)
+        {
+            if(a.negative && !b.negative)
+            {
+                a.negative = false;
+                return b - a;
+            }
+            else if(!a.negative && b.negative)
+            {
+                b.negative = false;
+                return a - b;
+            }
+            else if(a.negative && b.negative)
+            {
+                a.negative = false;
+                b.negative = false;
+                return new BigNumber("-1") * (a + b);
+            }
+
+            string s1 = new string(a.number.Reverse().ToArray());
+            string s2 = new string(b.number.Reverse().ToArray());
+            if (s1.Length > s2.Length)
+            {
+                int i = s1.Length - s2.Length;
+                for (; i > 0; i--)
+                {
+                    s2 += "0";
+                }
+            }
+            else
+            {
+                int i = s2.Length - s1.Length;
+                for (; i > 0; i--)
+                {
+                    s1 += "0";
+                }
+            }
+            string result = "";
+            int toAdd = 0;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                result += ((Convert.ToInt32(s1[i].ToString()) + Convert.ToInt32(s2[i].ToString()) + toAdd) % 10).ToString();
+                toAdd = (Convert.ToInt32(s1[i].ToString()) + Convert.ToInt32(s2[i].ToString()) + toAdd) / 10;
+            }
+            if (toAdd == 1)
+                result += '1';
+            return new BigNumber(new string(result.Reverse().ToArray()));
+        }
+
+        public static bool operator <(BigNumber a, BigNumber b)
+        {
+            if (a.number.Length < b.number.Length && a.negative == b.negative)
+                return true;
+            if (a.number.Length > b.number.Length && a.negative == b.negative)
+                return false;
+            if (a.negative && !b.negative)
+                return true;
+            if (!a.negative && b.negative)
+                return false;
+            for (int i = 0; i < a.number.Length; i++)
+            {
+                if (Convert.ToInt32(a.number[i].ToString()) < Convert.ToInt32(b.number[i].ToString())) return true;
+                if (Convert.ToInt32(a.number[i].ToString()) > Convert.ToInt32(b.number[i].ToString())) return false;
+            }
+            return false;
+        }
+
+        public static bool operator >(BigNumber a, BigNumber b)
+        {
+            if (a.number.Length < b.number.Length && a.negative == b.negative)
+                return false;
+            if (a.number.Length > b.number.Length && a.negative == b.negative)
+                return true;
+            if (a.negative && !b.negative)
+                return false;
+            if (!a.negative && b.negative)
+                return true;
+            for (int i = 0; i < a.number.Length; i++)
+            {
+                if (Convert.ToInt32(a.number[i].ToString()) > Convert.ToInt32(b.number[i].ToString())) return true;
+                if (Convert.ToInt32(a.number[i].ToString()) < Convert.ToInt32(b.number[i].ToString())) return false;
+            }
+            return false;
+        }
+
+        public static bool operator ==(BigNumber a, BigNumber b)
+        {
+            return !(a > b) && !(a < b);
+        }
+
+        public static bool operator !=(BigNumber a, BigNumber b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator >=(BigNumber a, BigNumber b)
+        {
+            return (a > b) || (a == b);
+        }
+
+        public static bool operator <=(BigNumber a, BigNumber b)
+        {
+            return (a < b) || (a == b);
+        }
+
+        public static BigNumber operator -(BigNumber a)
+        {
+            return new BigNumber(a.number, true);
+        }
+
+        public static BigNumber operator -(BigNumber a, BigNumber b)
+        {
+            if(a.negative && b.negative)
+            {
+                a.negative = false;
+                b.negative = false;
+                return b - a;
+            }
+            else if(!a.negative && b.negative)
+            {
+                b.negative = false;
+                return a + b;
+            }
+            else if(a.negative && !b.negative)
+            {
+                a.negative = false;
+                return new BigNumber("-1") * (a + b);
+            }
+
+            if (b > a)
+                return new BigNumber((b - a).number, true);
+
+            string s1 = new string(a.number.Reverse().ToArray());
+            string s2 = new string(b.number.Reverse().ToArray());
+            if (s1.Length > s2.Length)
+            {
+                int i = s1.Length - s2.Length;
+                for (; i > 0; i--)
+                {
+                    s2 += "0";
+                }
+            }
+            else
+            {
+                int i = s2.Length - s1.Length;
+                for (; i > 0; i--)
+                {
+                    s1 += "0";
+                }
+            }
+            string result = "";
+            int toAdd = 0;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if ((Convert.ToInt32(s1[i].ToString()) - Convert.ToInt32(s2[i].ToString()) - toAdd) < 0)
+                {
+                    result += ((Convert.ToInt32(s1[i].ToString()) - Convert.ToInt32(s2[i].ToString()) + 10 - toAdd) % 10).ToString();
+                    toAdd = 1;
+                }
+                else
+                {
+                    result += ((Convert.ToInt32(s1[i].ToString()) - Convert.ToInt32(s2[i].ToString()) - toAdd) % 10).ToString();
+                    toAdd = 0;
+                }
+            }
+            if (toAdd == -1)
+                result += "1-";
+            return new BigNumber(new string(result.Reverse().ToArray()));
+        }
+
+        public static BigNumber operator *(BigNumber a, BigNumber b)
+        {
+            string s1 = new string(a.number.Reverse().ToArray());
+            string s2 = new string(b.number.Reverse().ToArray());
+            if (s1.Length > s2.Length)
+            {
+                int i = s1.Length - s2.Length;
+                for (; i > 0; i--)
+                {
+                    s2 += "0";
+                }
+            }
+            else
+            {
+                int i = s2.Length - s1.Length;
+                for (; i > 0; i--)
+                {
+                    s1 += "0";
+                }
+            }
+            string q = "";
+            BigNumber answer = new BigNumber("0");
+            for (int i = 0; i < s1.Length; i++)
+            {
+                int toAdd = 0;
+                string result = "";
+                for (int j = 0; j < s2.Length; j++)
+                {
+                    result += ((Convert.ToInt32(s1[i].ToString()) * Convert.ToInt32(s2[j].ToString()) + toAdd) % 10).ToString();
+                    toAdd = (Convert.ToInt32(s1[i].ToString()) * Convert.ToInt32(s2[j].ToString()) + toAdd) / 10;
+                }
+                if (toAdd > 0)
+                {
+                    result += new string(toAdd.ToString().Reverse().ToArray());
+                }
+                answer = answer + new BigNumber(new string(result.Reverse().ToArray()) + q);
+                q += "0";
+            }
+            if ((a.negative && !b.negative) || (!a.negative && b.negative))
+                answer.negative = true;
+            return answer;
+        }
+
+        public static BigNumber operator /(BigNumber a, BigNumber b)
+        {
+            bool neg = (a.negative && !b.negative) || (!a.negative && b.negative);
+            a.negative = false;
+            b.negative = false;
+
+            if (b.number == "0")
+                throw new DivideByZeroException();
+            if (a < b) return new BigNumber("0");
+            string left = a.number.Substring(0, b.number.Length);
+            string result = "";
+            for (int i = b.number.Length; i <= a.number.Length; i++)
+            {
+                int ok = 0;
+                for (int j = 1; j <= 9; j++)
+                {
+                    if (b * new BigNumber(j.ToString()) <= new BigNumber(left))
+                        ok = j;
+                    else
+                        break;
+                }
+                result += ok.ToString();
+                if (i < a.number.Length)
+                    left = (new BigNumber(left) - b * new BigNumber(ok.ToString())).number + a.number[i];
+            }
+
+            BigNumber answer = new BigNumber(result);
+            answer.negative = neg;
+
+            return answer;
+        }
+
+        public static BigNumber operator %(BigNumber a, BigNumber b)
+        {
+            if (b.number == "0")
+                throw new DivideByZeroException();
+            return a - (a / b) * b;
+        }
+
+        public static BigNumber mod(BigNumber a, BigNumber b)
+        {
+            return a % b;
+        }
+
+        public static BigNumber operator ^(BigNumber a, BigNumber b)
+        {
+            //b to base of 2
+            string b_bin = "";
+            while(b > new BigNumber("0"))
+            {
+                b_bin = (b % new BigNumber("2")).number + b_bin;
+                b = b / new BigNumber("2");
+            }
+
+            string res = a.number;
+
+            for(int i = 1; i<b_bin.Length; ++i)
+            {
+                BigNumber temp = new BigNumber(res);
+                temp = temp * temp;
+                if (b_bin[i] == '1')
+                {
+                    temp = temp * a;
+                }
+                res = temp.number;
+            }
+
+            return new BigNumber(res);
+        }
+
+        public static BigNumber Shift(BigNumber a)
+        {
+            if(a == new BigNumber("2"))
+            {
+                return new BigNumber("1");
+            }
+            if(a == new BigNumber("1"))
+            {
+                return new BigNumber("0");
+            }
+            string a_bin = "";
+            while (a > new BigNumber("0"))
+            {
+                a_bin = (a % new BigNumber("2")).number + a_bin;
+                a = a / new BigNumber("2");
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append("0");
+            sb.Append(a_bin.Substring(0, a_bin.Length - 1));
+            string s = sb.ToString();
+            BigNumber res = new BigNumber("0");
+            for(int i = 0; i < s.Length; ++i)
+            {
+                if(s[i] == '1')
+                {
+                    BigNumber temp = new BigNumber("2") ^ new BigNumber((s.Length - i - 1).ToString());
+                    res = res + temp;
+                }
+            }
+            return res;
+        }
+
+        public static BigNumber ModExp(BigNumber x, BigNumber y, BigNumber n)
+        {
+            if(y == new BigNumber("0"))
+            {
+                return new BigNumber("1");
+            }
+            BigNumber z = ModExp(x, y / new BigNumber("2"), n);
+            if(y % new BigNumber("2") == new BigNumber("0"))
+            {
+                return (z * z) % n;
+            }
+            else
+            {
+                return (x * z * z) % n;
+            }
+        }
+
+        public static BigNumber GCD(BigNumber a, BigNumber b)
+        {
+            BigNumber zero = new BigNumber("0");
+            while (a > zero && b > zero)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+
+        public static string IntSqrt(BigNumber a)
+        {
+            if(a.negative)
+            {
+                Console.WriteLine("Square root for a negative value does not exist!");
+                return "";
+            }
+
+            return sq(a).number;
+        }
+        
+        public static BigNumber sq(BigNumber x)
+        {
+            if (x < new BigNumber("4"))
+            {
+                return new BigNumber("1");
+            }
+            BigNumber r = new BigNumber("2") * sq((x - x % new BigNumber("4")) / new BigNumber("4"));
+            if ((r + new BigNumber("1"))* (r + new BigNumber("1")) <= x)
+            {
+                return r + new BigNumber("1");
+            }
+            else
+            {
+                return r;
+            }
+        }
+
+        public static BigNumber Core(BigNumber x, BigNumber k)
+        {
+            if(x < k)
+            {
+                return new BigNumber("0");
+            }
+            return new BigNumber("1") + Core(x - k, k + new BigNumber("2"));
+        }
+
+        public static BigNumber LCM(BigNumber a, BigNumber b)
+        {
+            return a / GCD(a, b) * b;
+        }
+
+        public static BigNumber modPow(BigNumber num, BigNumber pow, BigNumber mod)
+        {
+            BigNumber test;
+            BigNumber n = num;
+            for (test = new BigNumber("1"); pow != new BigNumber("0"); pow = Shift(pow))
+            {
+                if (pow.number[pow.number.Length - 1] == '1')
+                    test = ((test % mod) * (n % mod)) % mod;
+                n = ((n % mod) * (n % mod)) % mod;
+            }
+
+            return test; /* note this is potentially lossy */
+        }
+
+        public override string ToString()
+        {
+            return negative? "-" + number : number;
+        }
+    }
+}
---
+++ BigIntegerImplementation/Program.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BigIntegerImplementation
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose an action to perform: " +
+                    "\n1 - process basic operations" +
+                    "\n2 - process operations by module" +
+                    "\n3 - solve system of comparsions" +
+                    "\n-----type \"stop\" when finished-----");
+
+                int p;
+                string operation = Console.ReadLine();
+                if(operation.Equals("stop"))
+                {
+                    return;
+                }
+
+                Int32.TryParse(operation, out p);
+
+                if (p == 1)
+                {
+                    basicOperations();
+                }
+                else if (p == 2)
+                {
+                    moduleOperaions();
+                }
+                else if (p == 3)
+                {
+                    comparsionSystem();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+        }
+
+        private static void comparsionSystem()
+        {
+            Regex regex = new Regex(@"^-?[0-9]+$");
+            Console.WriteLine("Enter comparsion system as the following: \nA B (x = A(mod B))");
+            Console.WriteLine("Print \"done\" when finished");
+            string s = "";
+            List<BigNumber> b = new List<BigNumber>();
+            List<BigNumber> m = new List<BigNumber>();
+            while(!s.Equals("done"))
+            {
+                s = Console.ReadLine();
+                if(s.Split(' ').Length != 2)
+                {
+                    if(s.Equals("done"))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Invalid syntax");
+                    continue;
+                }
+                if(regex.IsMatch(s.Split(' ')[0]) && regex.IsMatch(s.Split(' ')[1]))
+                {
+                    b.Add(new BigNumber(s.Split(' ')[0]));
+                    m.Add(new BigNumber(s.Split(' ')[1]));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid syntax!");
+                }
+            }
+
+            List<BigNumber> listM = new List<BigNumber>();
+            BigNumber M = new BigNumber("1");
+            foreach(BigNumber element in m)
+            {
+                M = M * element;
+            }
+
+            foreach (BigNumber element in m)
+            {
+                listM.Add(M / element);
+            }
+
+            List<BigNumber> listN = new List<BigNumber>();
+
+            for(int i = 0; i<listM.Count; ++i)
+            {
+                try
+                {
+                    listN.Add(extendedEuclid(listM[i], m[i]));
+                }
+                catch(DivideByZeroException ex)
+                {
+                    Console.WriteLine("Multiplicative inverse does not exist.");
+                    return;
+                }
+            }
+
+            BigNumber x = new BigNumber("0");
+            for(int i = 0; i<m.Count; ++i)
+            {
+                x = x + b[i] * listN[i] * listM[i];
+            }
+
+            Console.WriteLine($"x = {x.GetNumber()}") ;
+        }
+
+        private static void moduleOperaions()
+        {
+            Regex regex = new Regex(@"^-?[0-9]+$");
+            Console.WriteLine("Enter a module: ");
+            string module = Console.ReadLine();
+
+            if (!regex.IsMatch(module))
+            {
+                Console.WriteLine("NaN");
+                return;
+            }
+
+            Console.WriteLine("Actions available: + - / * ^\nEnter an expression: ");
+            string s = Console.ReadLine();
+
+            if (s.Split(' ').Length == 3)
+            {
+                if (!regex.IsMatch(s.Split(' ')[0]) || !regex.IsMatch(s.Split(' ')[2]))
+                {
+                    Console.WriteLine("NaN");
+                    return;
+                }
+
+                BigNumber a = new BigNumber(s.Split(' ')[0]);
+                BigNumber b = new BigNumber(s.Split(' ')[2]);
+                BigNumber m = new BigNumber(module);
+
+                switch (s.Split(' ')[1])
+                {
+                    case "+":
+                        BigNumber temp = a + b;
+                        if (a + b >= m)
+                            temp = temp - m;
+                        Console.WriteLine(temp.GetNumber(), "(mod", m, ")");
+                        break;
+                    case "-":
+                        temp = a + b;
+                        if (a - b < new BigNumber("0"))
+                            temp = temp + m;
+                        Console.WriteLine(temp.GetNumber(), "(mod", m, ")");
+                        break;
+                    case "*":
+                        BigNumber temp_mult = (a * b) / m;
+                        Console.WriteLine(((a * b) - temp_mult*m).GetNumber(), "(mod",m,")");
+                        break;
+                    case "/":
+                        BigNumber h;
+                        try
+                        {
+                            h = extendedEuclid(b, m);
+                        }
+                        catch (DivideByZeroException ex)
+                        {
+                            return;
+                        }
+                        //Console.WriteLine(h);
+                        temp_mult = (a * h) / m;
+                        Console.WriteLine(((a * h) - temp_mult * m).GetNumber(), "(mod", m, ")");
+                        break;
+                    case "^":
+                        Console.WriteLine(BigNumber.ModExp(a, b, m));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid syntax!");
+                return;
+            }
+        }
+
+        private static BigNumber extendedEuclid(BigNumber b, BigNumber m)
+        {
+            BigNumber x1 = new BigNumber("1");
+            BigNumber x2 = new BigNumber("0");
+            BigNumber x3 = m;
+            BigNumber y1 = new BigNumber("0");
+            BigNumber y2 = new BigNumber("1");
+            BigNumber y3 = b;
+
+            while(y3 != new BigNumber("0") && y3 != new BigNumber("1"))
+            {
+                BigNumber g = x3 / y3;
+                BigNumber t1 = x1 - g * y1;
+                BigNumber t2 = x2 - g * y2;
+                BigNumber t3 = x3 - g * y3;
+
+                x1 = y1;
+                x2 = y2;
+                x3 = y3;
+
+                y1 = t1;
+                y2 = t2;
+                y3 = t3;
+            }
+
+            if(y3 == new BigNumber("0"))
+            {
+                Console.WriteLine("Multiplicative inverse does not exist.");
+                throw new DivideByZeroException();
+            }
+            else
+            {
+                return y2;
+            }
+        }
+
+        public static void basicOperations()
+        {
+            Console.WriteLine("Actions available: + - / * ^ > < == IntSqrt\nEnter an expression: ");
+            string s = Console.ReadLine();
+            Regex regex = new Regex(@"^-?[0-9]+$");
+
+            if (s.Split(' ').Length == 2)
+            {
+                if (!regex.IsMatch(s.Split(' ')[0]))
+                {
+                    Console.WriteLine("NaN");
+                    return;
+                }
+                BigNumber x = new BigNumber(s.Split(' ')[0]);
+                if (s.Split(' ')[1] == "IntSqrt")
+                {
+                    try
+                    {
+                        Console.WriteLine(BigNumber.IntSqrt(x));
+                        return;
+                    }
+                    catch (StackOverflowException e)
+                    {
+                        Console.WriteLine("Number is too big for this operation!");
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Syntax");
+                }
+                return;
+            }
+            else if (s.Split(' ').Length == 3)
+            {
+                if (!regex.IsMatch(s.Split(' ')[0]) || !regex.IsMatch(s.Split(' ')[2]))
+                {
+                    Console.WriteLine("NaN");
+                    return;
+                }
+                BigNumber a = new BigNumber(s.Split(' ')[0]);
+                BigNumber b = new BigNumber(s.Split(' ')[2]);
+                switch (s.Split(' ')[1])
+                {
+                    case "+":
+                        Console.WriteLine((a + b));
+                        break;
+                    case "-":
+                        Console.WriteLine((a - b));
+                        break;
+                    case "*":
+                        Console.WriteLine((a * b));
+                        break;
+                    case "/":
+                        Console.WriteLine((a / b));
+                        break;
+                    case "%":
+                        Console.WriteLine((a % b));
+                        break;
+                    case "^":
+                        Console.WriteLine((a ^ b));
+                        break;
+                    case ">":
+                        Console.WriteLine((a > b));
+                        break;
+                    case "<":
+                        Console.WriteLine((b > a));
+                        break;
+                    case "==":
+                        Console.WriteLine((b == a));
+                        break;
+                    case "GCD":
+                        Console.WriteLine(BigNumber.GCD(a, b));
+                        break;
+                    case "LCM":
+                        Console.WriteLine(BigNumber.LCM(a, b));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid syntax!");
+                return;
+            }
+        }
+    }
+}
